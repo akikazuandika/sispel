2489cf1172a317aae17cfdf6f96f2912f5dcd206
Caca Wilda Andika <akikazuandika@gmail.com>
2019-09-01 11:28:42 +0700

    Change template and slicing

--- application/modules/admin/controllers/Room.php
+++ application/modules/admin/controllers/Room.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+class Room extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
@@ -11,15 +11,7 @@ class Admin extends CI_Controller {
 	public function index()
 	{
 		$data['title'] = "Dashboard | Admin";
-		$this->load->view('part/header', $data);
 		$this->load->view('index', $data);
-		$this->load->view('part/footer', $data);
-	}
-
-	public function logout()
-	{
-		session_destroy();
-		header("Location:/admin/login");
 	}
 
 }
